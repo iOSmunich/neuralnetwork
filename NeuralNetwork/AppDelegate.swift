daf2450199935d12b5cea07afcade9f02b154e3a
admin <admin@admins-imac.localdomain>
2016-05-05 14:56:58 +0200

    Initial Commit

---
+++ NeuralNetwork/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  NeuralNetwork
+//
+//  Created by admin on 16/5/5.
+//  Copyright © 2016年 admin. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
